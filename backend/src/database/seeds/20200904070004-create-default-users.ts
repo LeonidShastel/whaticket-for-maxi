53ceedbeb02724bb2a88d563bad1cdbcacfccace
Laxable <thelaxab@ya.ru>
2025-07-06 17:48:26 +0300

    Predeploy

---
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -0,0 +1,25 @@
+import { QueryInterface } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "Administrador",
+          email: "admin@whaticket.com",
+          passwordHash: "$2b$08$F2o2keBVHMezzBslZxe1vunRa98OATUBvamHikx9NR7t8iJEmS7bW",
+          profile: "admin",
+          tokenVersion: 0,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkDelete("Users", {});
+  }
+};
